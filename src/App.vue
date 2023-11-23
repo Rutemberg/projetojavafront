072f0e67545f91de3cafa4689da44b820a13baf0
Rutemberg <rutemberg@moneycloud.com.br>
2023-11-22 22:37:52 -0300

    Listar produtos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+
+    <v-app-bar color="#B81014" flat height="72">
+      <v-app-bar-title><v-img width="100" src="./assets/Rubao.png"></v-img></v-app-bar-title>
+    </v-app-bar>
+
+    <v-footer app color="#F4C808" height="20"></v-footer>
+
+    <v-navigation-drawer expand-on-hover rail>
+      <v-list>
+        <v-list-item prepend-icon="mdi-home" title="Home" to="/"></v-list-item>
+        <v-list-group>
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-package-variant-closed" title="Produtos"></v-list-item>
+          </template>
+          <v-list-item v-for="([title, icon, link], i) in crud_produto" :key="i" :value="title" :title="title"
+            :prepend-icon="icon" :to="link"></v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
@@ -12,9 +32,12 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'App',
 
-  data () {
+  data() {
     return {
-      //
+      crud_produto: [
+        ['Cadastrar', 'mdi-plus', "/"],
+        ['Listar', 'mdi-view-list', "/produtos"],
+      ]
     }
   },
 })
---
+++ src/views/Produto/ListarView.vue
@@ -0,0 +1,19 @@
+<template>
+    <listar-produto />
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Components
+import ListarProduto from '../../components/Produto/ListarProduto.vue';
+
+export default defineComponent({
+    name: 'ListarView',
+
+    components: {
+        ListarProduto,
+    },
+});
+</script>
+  
